f6ef2db615fcdfd5ebd6f80f42e452f53455dc28
Samar Sajnani <ssajnani@uwo.ca>
2019-06-24 23:29:17 -0400

    fixes

--- stack/setting.js
+++ stack/setting.js
@@ -1,31 +1,58 @@
 
 
 $(document).ready(function () {
-    function openClose(e) {
-        if (e.key != "Escape"){
-            console.log(e);
-            if ($('#openClose').val() == "Listening"){
-                console.log('here');
-                $('#openClose').val(e.key);
-            } else {
-                var prev = $('#openClose').val();
-                if (prev[-1] == prev[-2]){
-                    prev = prev.slice(0, -1);
+    function keyDown(shortcut) {
+        return function curried_func(e) {
+            var settings = ls('settings');
+            if (e.key != "Escape"){
+
+                if ($('#' + shortcut).val() == "Listening"){
+                    $('#' + shortcut).val(e.key);
+                    settings[shortcut] = e.key;
+                } else {
+                    if (!$('#' + shortcut).val().split('+').includes(e.key)){
+                        var sc = $('#' + shortcut).val() + "+" + e.key;
+                        $('#' + shortcut).val(sc);
+                        settings[shortcut] = sc;
+                    }
                 }
-                console.log(e.key);
-                $('#openClose').val(prev + "+" + e.key);
+                ls('settings', settings);
+            } else {
+                window.removeEventListener('keydown', keyDown(shortcut));
             }
-        } else {
-            window.removeEventListener('keydown');
         }
-      }
+    }
     getCreateSettings(function(err, result){
-        $('#openClose').val(result.openClose);
-        $('#openClose').focusin(function(){
-            $('#openClose').val('Listening');
-            window.addEventListener('keydown', openClose);
-        });
+        $.each(ls('settings'), function(i, val){
+
+            $(".tab").append(
+                `<div class="taskInput">
+                        <label for="tname">`+ i.match(/[A-Z][a-z]+/g).map(x => x).join(' ').trim() +`</label><br><br>
+                        <input readonly id="` + i +`" type="text" name="openClose" value="`+val+`">
+                    </div><br></br>`
+            );
+            $('#' + i).focusin(function(){
+                $('#' + i).val('Listening');
+                var curried = keyDown(i);
+                window.addEventListener('keydown', curried);
+                $('#' + i).focusout(function(){
+                    if($('#' + i).val() == "Listening"){
+                        $('#' + i).val(result[i]);
+                    }
+                    window.removeEventListener('keydown', curried);
+                });
+            });
+            
+          });
+        
     });
-    
+    $('#updateSettings').on('submit', function(){
+        updateSettings(function(err, result){
+            if(err){
+                console.log(err);
+            }
+        })
+        window.location.replace('./stack.html');
+    });
   
 });
--- stack/settings.js
+++ stack/settings.js
@@ -1,5 +1,16 @@
 var stackFunctions = require('../helper/stack_functions');
 
+function updateSettings(callback){
+  githubFunctions.createUpdateFile(ls('token'), ls('username'), ls('repoName'), "settings", JSON.stringify(ls('settings')), function (err, result) {
+    if (err) {
+      $('#errorreponame').text("Cannot create a settings file in the repository: " + err.message);
+      return callback(err.message, null)
+    } else {
+      return callback(null, "success")
+    }
+  });
+}
+
 function getCreateSettings(callback) {
   githubFunctions.checkFileExists(ls('token'), ls('username'), ls('repoName'), "settings", function (err, contentArray) {
     if (err == null && contentArray === "") {
@@ -24,37 +35,41 @@ function getCreateSettings(callback) {
   });
 }
 function setGlobalVariables(settingsObject) {
-  globalShortcut.unregister(settingsObject.openClose);
-  globalShortcut.unregister(settingsObject.newTask);
-  globalShortcut.register(settingsObject.openClose, function () {
+  globalShortcut.unregister(settingsObject.OpenCloseWindow);
+  globalShortcut.unregister(settingsObject.NewTask);
+  globalShortcut.unregister(settingsObject.ClockIn);
+  globalShortcut.unregister(settingsObject.ClockOut);
+  globalShortcut.unregister(settingsObject.Settings);
+  globalShortcut.register(settingsObject.OpenCloseWindow, function () {
     if (remote.getCurrentWindow().isVisible()) {
       remote.getCurrentWindow().hide();
     } else {
       remote.getCurrentWindow().show();
     }
   });
-  globalShortcut.register(settingsObject.newTask, function () {
+  globalShortcut.register(settingsObject.NewTask, function () {
       window.location.replace("./createTask.html");
     });
-  globalShortcut.register(settingsObject.clockIn, function () {
+  globalShortcut.register(settingsObject.ClockIn, function () {
     stackFunctions.clockIn(0);
   });
-  globalShortcut.register(settingsObject.clockOut, function () {
+  globalShortcut.register(settingsObject.ClockOut, function () {
     stackFunctions.clockOut();
   });
 
-  globalShortcut.register('alt+o', function () {
+  globalShortcut.register(settingsObject.Settings, function () {
     window.location.replace('./settings.html');
   });
     
 }
 var settings = {
-  "openClose": "alt+z",
-  "nextPageTask": "Alt+Down",
-  "prevPageTask": "Alt+Up",
-  "newTask": "alt+n",
-  "clockIn": "alt+c",
-  "clockOut": "alt+v"
+  "OpenCloseWindow": "alt+z",
+  "NextPageTask": "Alt+Down",
+  "PreviousPageTask": "Alt+Up",
+  "NewTask": "alt+n",
+  "ClockIn": "alt+c",
+  "ClockOut": "alt+v", 
+  "Settings": "alt+s"
 };
 
 $(document).ready(function () {
